1a0d48970967032ba9842985b9dbf9a86a885b7f
KayhanQ <kayhanfqaiser@gmail.com>
2016-01-21 23:25:19 -0500

    Add Q sequence with variable number of function applications instead of just 2

--- sequence.py
+++ sequence.py
@@ -8,4 +8,34 @@ def q(n):
 		i = i+1
 	print seq
 
-q(1000)
+
+def l(n):
+	a, b = 1, 1
+	seq = [a,b]
+	i = 2
+
+	f = lambda seq, i, r: seq[i-r]
+	while i < n:
+		
+		times = seq[i-1]
+		t1 = 1
+		for index in range(0,times):
+			t1 = f(seq, i, t1)
+
+		times = seq[i-2]
+		t2 = 2
+		for index in range(0,times):
+			t2 = f(seq, i, t2)
+
+		#c = f(seq, i, f(seq, i, 1)) + f(seq, i, f(seq, i, 2))
+		c = t1+t2
+		seq.append(c)
+		i = i+1
+		print c
+	print seq
+
+
+
+
+#q(1000)
+l(5000)
