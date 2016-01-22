b9ccec600807fe3d998cc6e20af0845c174cc1f3
KayhanQ <kayhanfqaiser@gmail.com>
2016-01-22 01:13:15 -0500

    cleanup

--- Q_cs.py
+++ Q_cs.py
@@ -4,10 +4,8 @@ def l(n):
 	i = 2
 
 	while i < n:
-		times = seq[i-1]
 		t1 = apply_func(seq, i, 1, seq[i-1])
 		t2 = apply_func(seq, i, 2, seq[i-2])
-
 		c = t1+t2
 		seq.append(c)
 		i = i+1
@@ -19,5 +17,4 @@ def apply_func(seq, i, r, times):
 		r = seq[i-r]
 	return r
 
-#q(1000)
-l(5000)
+l(100)
